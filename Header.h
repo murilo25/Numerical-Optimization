bdc6f66d295c06dd997391f1abdf8f297bf3c3df
Murilo Pinheiro <muriloaugusto25@hotmail.com>
2020-12-04 19:14:25 -0600

    Add Header file

---
+++ Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+double optmization (double args, uint8_t method)
+{
+	return 0;
+}
--- SteepestDescent.cpp
+++ SteepestDescent.cpp
@@ -2,10 +2,11 @@
 // 04-Dec-2020
 // Solution for Diamond Kinetics - Dynamics Algorithm Implementation.pdf
 // SteepestDescent.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Header.h : This files contains all functions and includes used in this file.
 
 
-#include <iostream>
-#include <math.h>
+
+#include "Header.h"
 
 int main()
 {
