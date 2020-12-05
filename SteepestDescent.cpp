fdd595161c03d13202214c413fa475bc8de04b2f
murilo25 <42446108+murilo25@users.noreply.github.com>
2020-12-04 18:51:18 -0600

    Initial commit

---
+++ SteepestDescent.cpp
@@ -0,0 +1,14 @@
+// Murilo Augusto Pinheiro
+// 04-Dec-2020
+// Solution for Diamond Kinetics - Dynamics Algorithm Implementation.pdf
+// SteepestDescent.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
